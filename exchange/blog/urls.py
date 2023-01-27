03428cfe11c2c1e0eabfc06e76b0d45ca61ceae7
MRX580 <andrejchenko0511@mail.ru>
2023-01-28 00:58:48 +0100

    creating system "Add blog"

--- exchange/blog/urls.py
+++ exchange/blog/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='blog'),
-    path('<int:pk>', blog, name='blog'),
+    path('<int:pk>/', blog, name='blog'),
+    path('create_blog/', create_blog, name='create_blog'),
 ]
--- exchange/blog/views.py
+++ exchange/blog/views.py
@@ -1,12 +1,32 @@
 from django.shortcuts import render
 from .models import Blog
+from .forms import BlogForm
+from django.contrib import messages
+from django.core.exceptions import ValidationError
 
 
 def index(request):
-    get_blog = Blog.objects.all()
+    get_blog = Blog.objects.all()[::-1]
     return render(request, 'blog/index.html', {'data_blog': get_blog})
 
 
 def blog(request, pk):
     get_blog = Blog.objects.get(pk=pk)
     return render(request, 'blog/blog.html', {'data_blog': get_blog})
+
+
+def create_blog(request):
+    if not request.user.is_staff:
+        raise ValidationError('Шо?')
+    if request.method == 'POST':
+        form = BlogForm(request.POST, request.FILES)
+        if form.is_valid():
+            name = form.save()
+            name.save()
+            messages.success(request, 'Блог был успешно опубликован!')
+        else:
+            for error in list(form.errors.values()):
+                messages.error(request, error)
+    else:
+        form = BlogForm()
+    return render(request, 'blog/create_blog.html', {'form': form})
