aaf6cc5c6a013f3e633e9c5096c0df4fae01da49
MRX580 <andrejchenko0511@mail.ru>
2023-01-30 23:14:48 +0100

    add image to blog

--- exchange/exchange/urls.py
+++ exchange/exchange/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from .settings import DEBUG, MEDIA_URL, MEDIA_ROOT
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,4 +10,4 @@ urlpatterns = [
     path('chart/', include('charts.urls')),
     path('blog/', include('blog.urls')),
     path('wallet/', include('main.urls')),
-]
+] + static(MEDIA_URL, document_root=MEDIA_ROOT)
